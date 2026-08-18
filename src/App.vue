9c5cfedda061d83dd915f779d43c366ed7fa7cfe
David Bailly <david.bailly.dev@proton.me>
2026-08-18 21:58:17 +0200

    feat: ajouter liste taches avec v-for et message conditionnel

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,20 @@ import { ref } from 'vue'
 
 const counter = ref(0)
 const myInput = ref('')
+const defaultTasks = [
+  {
+    id: 1,
+    text: 'Laver la voiture'
+  },
+  {
+    id: 2,
+    text: 'Acheter croquettes'
+  },{
+    id: 3,
+    text: 'Commander cadeau anniversaire'
+  }
+]
+const tasks = ref(defaultTasks)
 
 function getBackground() {
   if (myInput.value.length > 1) {
@@ -11,11 +25,26 @@ function getBackground() {
     return 'background-color: red'
   }
 }
+
+function toggleTasksData() {
+  if (tasks.value.length > 0) {
+    tasks.value = []
+  } else {
+    tasks.value = defaultTasks
+  }
+}
+
 </script>
 
 <template>
   <h1>Vue Dojo</h1>
   <button type="button" @click="counter++">{{ counter }}</button>
   <input :style="getBackground()" @input="myInput = $event.target.value" />
-  <p>myInput = {{ myInput }}</p>
+  <button @click="toggleTasksData">Afficher/Masquer tâches</button>
+  <table v-if="tasks.length > 0">
+    <tr v-for="task in tasks" :key="task.id">
+      <td>{{ task.text }}</td>
+    </tr>
+  </table>
+  <p v-else>Aucune tâche à afficher.</p>
 </template>
